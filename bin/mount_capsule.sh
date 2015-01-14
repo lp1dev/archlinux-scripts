d51799a73088c8964b3834be9cbfd6b454e59d1e
lupin <mail@lupin.me>
2015-01-14 04:07:02 +0100

    Added screen scripts

--- bin/mount_capsule.sh
+++ bin/mount_capsule.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-sudo mount.cifs //192.168.1.93/Disk /media/TimeCapsule -o user=lupin,passwd=$1,sec=ntlm
+#!/bin/bash
+
+if [ $# != "2" ]
+then
+    echo "Usage :" $0 "path password"
+    exit
+else
+    mount.cifs //192.168.1.93/Disk $1 -o user=lupin,passwd=$2,sec=ntlm
+fi
---
+++ screen/dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --mode 1920x1080 --pos 1280x0 --rotate normal --output DVI-I-1 --off --output DVI-I-0 --off --output DVI-I-3 --off --output DVI-I-2 --mode 1920x1080 --pos 1280x0 --rotate normal
