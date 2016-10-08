23f24a26136370e4327a85871036f80b5e56b29b
rasbperry local <pi@lp1.eu>
2016-10-08 16:58:04 +0000

    updates for Rpi

--- bin/mount_capsule.sh
+++ bin/mount_capsule.sh
@@ -5,5 +5,5 @@ then
     echo "Usage :" $0 "path password"
     exit
 else
-    mount.cifs //192.168.1.93/tc $1 -o user=lupin,passwd=$2,sec=ntlm
+    mount -t cifs //192.168.1.93/tc $1 -o user=lupin,passwd=$2,sec=ntlm
 fi
