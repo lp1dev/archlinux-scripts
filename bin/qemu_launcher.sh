57e4b8b62a5e4d2f62c0ed87878b4ce9a49fb3ce
Lupin <lupin@lp1.eu>
2016-10-09 04:00:06 +0200

    rewritten qemu_launcher and added brightness script

--- bin/qemu_launcher.sh
+++ bin/qemu_launcher.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/bash
-if [ $# != "1" ]
+if [ $# -lt "2" ]
 then
-   echo "usage : ./launch.sh [install|vmware|spice]"
+   echo "usage : ./launch.sh [install|vmware|spice] file.img"
    exit
 fi
 
 if [ $1 == "install" ]
 then
-    qemu-system-x86_64 -machine pc,accel=kvm:tcg -cpu host -m 4096 -smp 2 -hda w81-disk -drive file=W81.iso,media=cdrom -boot d
+    if [ $# != "3" ]
+    then
+	echo $# "\n"
+	echo "usage : ./launch.sh [install|vmware|spice] file.img file.iso"
+    else
+	qemu-system-x86_64 -machine pc,accel=kvm:tcg -cpu host -m 4096 -smp 2 -hda $2 -drive file=$3,media=cdrom -boot d
+    fi
 elif [ $1 == "vmware" ]
 then
-    qemu-system-x86_64 -machine pc,accel=kvm:tcg -vga vmware -cpu host -m 4096 -smp 8 -hda w81-disk -drive media=cdrom -boot d
+    qemu-system-x86_64 -machine pc,accel=kvm:tcg -vga vmware -cpu host -m 4096 -smp 8 -hda $2 -drive media=cdrom -boot d
 elif [ $1 == "spice" ]
 then
-    qemu-system-x86_64 -machine pc,accel=kvm:tcg -vga qxl -cpu host -m 4096 -smp 8 -hda w81-disk -drive media=cdrom -boot d -spice port=5900,addr=0.0.0.0,disable-ticketing
+    qemu-system-x86_64 -machine pc,accel=kvm:tcg -vga qxl -cpu host -m 4096 -smp 8 -hda $2 -drive media=cdrom -boot d -spice port=5900,addr=0.0.0.0,disable-ticketing
 else
     echo "unknown parameter :" $1
 fi
