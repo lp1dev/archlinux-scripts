2a26d9ea5c51f82139cc123786025b03215e55f5
Jeremie A <mail@lp1.eu>
2019-01-17 17:06:21 +0100

    Added adb alias and three screens display

---
+++ screen/3screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --mode 1920x1080 --pos 0x104 --rotate normal --output eDP-1 --primary --mode 1600x900 --pos 4480x476 --rotate normal --output HDMI-2 --off --output HDMI-1 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1 --off
